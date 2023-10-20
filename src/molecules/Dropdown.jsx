e158c2272386c04608192fda5a1c62b55ab76789
EJ <eric_j_fra@yahoo.fr>
2023-10-20 20:07:27 +0200

    Changes:    - Accommodation dropdown dynamic

--- src/molecules/Dropdown.jsx
+++ src/molecules/Dropdown.jsx
@@ -1,19 +1,31 @@
-import Chevron from '../atoms/ChevronUp';
+import { useState } from 'react';
+import ChevronUp from '../atoms/ChevronUp';
+import ChevronDown from '../atoms/ChevronDown';
 import PropTypes from 'prop-types';
 
 const Dropdown = ({title, content, id}) => {
 
+  const [isOpen, setIsOpen] = useState(false)
+  const click = () => isOpen ? setIsOpen(false) : setIsOpen(true)
+
   return (
     <div className='dropdown_wrapper'>
+
       <div className="dropdown_container">
         <div className="dropdown_title">{title}</div>
-        <div className="dropdown_button"><Chevron /></div>
-      </div>
-      <div className="dropdown_contents_container">
-        <div className='dropdown_contents_text'>
-          {typeof content === 'string' ? content : content.map((el, i) => (<p key={i + id}>{el}</p>))}
+        <div className="dropdown_button" onClick={() => click()}>
+          { isOpen ? <ChevronUp /> : <ChevronDown /> }
         </div>
       </div>
+
+      {!isOpen ? null :
+        <div className="dropdown_contents_container">
+          <div className='dropdown_contents_text'>
+            {typeof content === 'string' ? content : content.map((el, i) => (<p key={i + id}>{el}</p>))}
+          </div>
+        </div>
+      }
+
     </div>
   )
 }
@@ -27,4 +39,5 @@ Dropdown.propTypes = {
   ]),
   id: PropTypes.string
 }
+
 export default Dropdown;
